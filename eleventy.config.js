2cd1f4877f7e2c17ce2d44d42086458e291b1e16
niborus <niborus@yunamio.de>
2025-05-11 19:44:48 +0200

    Use proper data file type

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,8 @@
+import yaml from "js-yaml";
+
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("styling.css");
     eleventyConfig.addPassthroughCopy("lazyload.js");
     eleventyConfig.addPassthroughCopy("anchor_expand.js");
+    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
 };
